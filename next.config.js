3896e9c1c34269932bc02eb2fe484d64b9d2a754
beton <betonprops4@gmail.com>
2024-09-28 13:25:34 +0300

    production ready v1

--- next.config.js
+++ next.config.js
@@ -7,23 +7,23 @@ const nextConfig = {
   images: {
     domains: ['localhost', '3.8.211.12','vivid-flowers-9f3564b8da.media.strapiapp.com'], // Add 'localhost' or other domains as needed
 
-    // remotePatterns: [
-    //   {
-    //     protocol: 'http',
-    //     hostname: '3.8.211.12',  // Add your Strapi server IP or domain
-    //     port: '1337',            // Add port if needed
-    //     pathname: '/uploads/**', // Define the path of the images
-    //   },
-    // ],
-  },
-  async rewrites() {
-    return [
+    remotePatterns: [
       {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap',
+        protocol: 'http',
+        hostname: '3.8.211.12',  // Add your Strapi server IP or domain
+        port: '1337',            // Add port if needed
+        pathname: '/uploads/**', // Define the path of the images
       },
-    ];
+    ],
   },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/sitemap.xml',
+  //       destination: '/api/sitemap',
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
